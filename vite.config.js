f6751ff62c690451a607610df280ce1f8d81118f
Melkid69 <167970274+Melkid45@users.noreply.github.com>
2025-02-12 02:21:41 +0600

    last

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path';
-
-export const pathResolver = (p) => resolve(__dirname, '.', p);
 
 // https://vitejs.dev/config/
 export default defineConfig({
