e8e0772d0dcf62fcda2a3430661a2de2d3d7818e
Melkid69 <167970274+Melkid45@users.noreply.github.com>
2025-02-12 01:56:26 +0600

    last

--- client/src/pages/Guy.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Guy() {
-  return (
-    <div>Guy</div>
-  )
-}
--- client/src/pages/RegistUser.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function RegistUser() {
-  return (
-    <div>RegistUser</div>
-  )
-}
