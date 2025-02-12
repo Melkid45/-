5882a33cfaab3c5aba98103eb6cc7f91f82c52c8
Melkid69 <167970274+Melkid45@users.noreply.github.com>
2025-02-12 11:48:49 +0600

    last

--- src/App.jsx
+++ src/App.jsx
@@ -7,34 +7,17 @@ import Footer from './components/Footer';
 import { publicRoutes } from './routes';
 import Home from './pages/Home';
 function App() {
-  const locomotiveScroll = new LocomotiveScroll({
-    lenisOptions: {
-      wrapper: window,
-      content: document.documentElement,
-      lerp: 0.1,
-      duration: 3,
-      orientation: 'vertical',
-      gestureOrientation: 'vertical',
-      smoothWheel: true,
-      smoothTouch: true,
-      wheelMultiplier: 0.7,
-      touchMultiplier: 2,
-      normalizeWheel: true,
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
-  },
-  });
 
   return (
     <>
     <Head/>
-    <BrowserRouter>
-            <Routes>
+    
+          <Routes>
             {publicRoutes.map(({path , Component}) => 
                 <Route key={path} path={path} element={<Component/>} exact/>
             )}
             <Route path="*" element={<Home />} />
         </Routes>
-    </BrowserRouter>
     <Footer/>
     </>
   )
